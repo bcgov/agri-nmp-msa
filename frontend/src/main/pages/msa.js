afda6d5cfa9e2091404c78403b76e492a9a14d1c
Robert Johnstone <rob@plasticviking.com>
2020-12-07 17:34:42 +0800

    stylistic edits

--- frontend/src/main/pages/msa.js
+++ frontend/src/main/pages/msa.js
@@ -85,7 +85,7 @@ const Msa = () => {
   }
 
   const formatDate = (v) => (v.format('l'));
-  const formatNumeric = (v) => (Math.round(v));
+  const formatNumeric = (v, e) => (Math.round(v * ((10 + Number.EPSILON) ** e)) / (10 ** e));
 
   const riskMap = {};
   riskMap.LOW = 'Low';
@@ -123,8 +123,8 @@ const Msa = () => {
                       return (
                         <tr key={`forecast-${v}`}>
                           <td>{formatDate(moment(s.associatedForecast.forDate))}</td>
-                          <td>{formatNumeric(s.next24 ? s.next24 : 0)}</td>
-                          <td>{formatNumeric(s.next72 ? s.next72 : 0)}</td>
+                          <td>{formatNumeric(s.next24 ? s.next24 : 0, 0)}</td>
+                          <td>{formatNumeric(s.next72 ? s.next72 : 0, 0)}</td>
                           <td className={`risk-${s.runoffRisk}`}>
                             {riskMap[s.runoffRisk]}
                           </td>
@@ -134,27 +134,28 @@ const Msa = () => {
                     return null;
                   })}
                   </tbody>
-                  <tfoot>
-                  <tr>
-                    {[{ i: 0, name: 'today' }, { i: 1, name: 'tomorrow' }].map((v) => {
-                      const s = selectedFeature.properties.forecasts.statistics[v.i];
-                      if (!!s) {
-                        const formatted24 = formatNumeric(s.next24 ? s.next24 : 0);
-                        const formatted72 = formatNumeric(s.next72 ? s.next72 : 0);
-                        const worksheetLink = pageCustomization.armLink.replace('{24}', formatted24).replace('{72}', formatted72);
-                        return (
-                          <td key={`assessment-${v.i}`} colSpan={2}>
-                            <a target="_" href={worksheetLink} className="worksheetLink">
-                              {`Complete a risk assessment for ${v.name}`}
-                            </a>
-                          </td>
-                        );
-                      }
-                      return (<td colSpan={2} />);
-                    })}
-                  </tr>
-                  </tfoot>
                 </table>
+
+                <div className="worksheetLinkFlexbox">
+
+                  {[{ i: 0, name: 'today' }, { i: 1, name: 'tomorrow' }].map((v) => {
+                    const s = selectedFeature.properties.forecasts.statistics[v.i];
+                    if (!!s) {
+                      const formatted24 = formatNumeric(s.next24 ? s.next24 : 0, 2);
+                      const formatted72 = formatNumeric(s.next72 ? s.next72 : 0, 2);
+                      const worksheetLink = pageCustomization.armLink.replace('{24}', formatted24).replace('{72}', formatted72);
+                      return (
+                        <div className="worksheetLinkFlexcard" key={`assessment-${v.i}`}>
+                          <a target="_" href={worksheetLink} className="worksheetLink">
+                            {`Complete a risk assessment for ${v.name}`}
+                          </a>
+                        </div>
+                      );
+                    }
+
+                  })}
+                </div>
+
               </Popup>
             )}
           </GeoJSON>
